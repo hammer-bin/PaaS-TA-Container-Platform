18f2e66191d80a1768283adb6f494907bb9458f7
suslmk <110075566+hammer-bin@users.noreply.github.com>
2022-09-27 06:24:12 +0900

    Initial Commit

---
+++ PaaS-TA Container Platform/PaaS_TA_Container_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaaS_TA_Container_PlatformApp.swift
+//  PaaS-TA Container Platform
+//
+//  Created by minkyuLee on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct PaaS_TA_Container_PlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
